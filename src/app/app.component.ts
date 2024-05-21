02413ba9ca59b6d8e689029ce71b476ea10e0a7f
isma <ismaelmartin302@gmail.com>
2024-05-21 13:24:42 +0200

    fontawesome instalado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faComment as farComment, faCalendar as farCalendar, faHeart as farHeart, faUser as farUser } from '@fortawesome/free-regular-svg-icons';
+import { faBars, faCalendar, faComment, faHeart, faHouse, faLink, faLocationDot, faMagnifyingGlass, faRetweet, faUser, faUserMinus, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
-}
+  constructor(library: FaIconLibrary) {
+    library.addIcons(farHeart, farUser, farComment, farCalendar);
+    library.addIcons(faHeart, faUser, faLink, faRetweet, faComment, faCalendar, faHouse, faUserPlus, faUserMinus, faMagnifyingGlass, faLocationDot, faBars);
+  }
+} 
