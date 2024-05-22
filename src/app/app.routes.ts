c95d89fdaa1c218da635025001ae6dce909b972c
isma <ismaelmartin302@gmail.com>
2024-05-22 14:29:58 +0200

    creando todas las rutas principales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { Error404Component } from './pages/error404/error404.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { NotificationsComponent } from './pages/notifications/notifications.component';
+import { ExploreComponent } from './pages/explore/explore.component';
+import { OptionsComponent } from './pages/options/options.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'search', component: ExploreComponent },
+    { path: 'notifications', component: NotificationsComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'options', component: OptionsComponent },
+    { path: '404', component: Error404Component },
+];
---
+++ src/app/pages/error404/error404.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error404',
+  standalone: true,
+  imports: [],
+  templateUrl: './error404.component.html',
+  styleUrl: './error404.component.scss'
+})
+export class Error404Component {
+
+}
