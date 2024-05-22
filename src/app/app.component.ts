ca7627c6b0e51008f44b6fdd65d3cb9105c64d3a
isma <ismaelmartin302@gmail.com>
2024-05-22 14:23:24 +0200

    sidebar creado sin funcionalidad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faComment as farComment, faCalendar as farCalendar, faHeart as farHeart, faUser as farUser } from '@fortawesome/free-regular-svg-icons';
-import { faBars, faCalendar, faComment, faHeart, faHouse, faLink, faLocationDot, faMagnifyingGlass, faRetweet, faUser, faUserMinus, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { faBell as farBell, faComment as farComment, faCalendar as farCalendar, faHeart as farHeart, faUser as farUser } from '@fortawesome/free-regular-svg-icons';
+import { faBars, faBell, faCalendar, faComment, faHeart, faHouse, faLink, faLocationDot, faMagnifyingGlass, faRetweet, faUser, faUserMinus, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 @Component({
@@ -14,7 +14,7 @@ import { SidebarComponent } from './layout/sidebar/sidebar.component';
 })
 export class AppComponent {
   constructor(library: FaIconLibrary) {
-    library.addIcons(farHeart, farUser, farComment, farCalendar);
-    library.addIcons(faHeart, faUser, faLink, faRetweet, faComment, faCalendar, faHouse, faUserPlus, faUserMinus, faMagnifyingGlass, faLocationDot, faBars);
+    library.addIcons(farHeart, farUser, farComment, farCalendar, farBell);
+    library.addIcons(faHeart, faUser, faLink, faRetweet, faComment, faCalendar, faBell, faHouse, faUserPlus, faUserMinus, faMagnifyingGlass, faLocationDot, faBars);
   }
 } 
