cf41addda84f84d156b940ade063eeae19c72feb
isma <ismaelmartin302@gmail.com>
2024-06-14 21:01:27 +0200

    actualizando creacion de eventos

--- src/app/records/event/publish/publish.component.ts
+++ src/app/records/event/publish/publish.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UsersApiService } from '../../../services/users-api.service';
 import { Router } from '@angular/router';
 import { IApiResponseEvent } from '../../../services/models/event-api.interface';
@@ -19,6 +19,7 @@ export class PublishComponent implements OnInit, OnDestroy {
   user: IApiResponseUser | null = null;
   eventForm: FormGroup;
   customCapacitySelected = false;
+  customPriceSelected = false;
   private userSubscription: Subscription | undefined;
 
   constructor(
@@ -27,19 +28,21 @@ export class PublishComponent implements OnInit, OnDestroy {
     private userService: UserService,
     private router: Router
   ) {
-      this.eventForm = this.fb.group({
-        name: ['', Validators.required],
-        description: ['', Validators.required],
-        location: ['', Validators.required],
-        price: [''],
-        capacity: [''],
-        category: [''],
-        picture: [''],
-        website: [''],
-        starts_at: ['', Validators.required],
-        finish_in: ['',],
-        user_id: ['', Validators.required] // Inicializar sin user_id
-      });
+    this.eventForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      location: ['', Validators.required],
+      price: ['', Validators.required],
+      capacity: [''],
+      category: [''],
+      picture: [''],
+      website: [''],
+      starts_at: ['', [Validators.required, this.dateValidator]],
+      finish_in: [''],
+      user_id: ['', Validators.required]
+    }, {
+      validators: this.finishDateAfterStartDateValidator
+    });
   }
 
   ngOnInit(): void {
@@ -49,7 +52,8 @@ export class PublishComponent implements OnInit, OnDestroy {
         console.log('User ID:', this.user.id);
         this.eventForm.patchValue({ user_id: this.user.id });
       } else {
-        console.error('User is not loaded');      }
+        console.error('User is not loaded');
+      }
     });
   }
 
@@ -81,5 +85,44 @@ export class PublishComponent implements OnInit, OnDestroy {
       this.eventForm.get('capacity')?.clearValidators();
       this.eventForm.get('capacity')?.updateValueAndValidity();
     }
+  }
+
+  onPriceChange(value: string): void {
+    this.customPriceSelected = value === 'custom';
+    if (this.customPriceSelected) {
+      this.eventForm.get('price')?.setValue('');
+      this.eventForm.get('price')?.setValidators(Validators.required);
+      this.eventForm.get('price')?.updateValueAndValidity();
+    } else {
+      this.eventForm.get('price')?.setValidators(null);
+      this.eventForm.get('price')?.updateValueAndValidity();
+    }
+  }
+  onStartDateChange(): void {
+    const startDateControl = this.eventForm.get('starts_at');
+    const finishDateControl = this.eventForm.get('finish_in');
+
+    if (startDateControl && finishDateControl) {
+      const startDate = new Date(startDateControl.value);
+      finishDateControl.setValidators([Validators.required, this.dateValidator, this.finishDateAfterStartDateValidator.bind(this)]);
+      finishDateControl.updateValueAndValidity();
+    }
+  }
+  dateValidator(control: AbstractControl): { [key: string]: boolean } | null {
+    const selectedDate = new Date(control.value);
+    const now = new Date();
+    if (selectedDate < now) {
+      return { 'invalidDate': true };
+    }
+    return null;
+  }
+
+  finishDateAfterStartDateValidator(group: FormGroup): { [key: string]: boolean } | null {
+    const startDate = group.get('starts_at')?.value;
+    const finishDate = group.get('finish_in')?.value;
+    if (startDate && finishDate && new Date(finishDate) < new Date(startDate)) {
+      return { 'finishDateBeforeStartDate': true };
+    }
+    return null;
   }
 }
